f7601d874645051666ac2556fe47d6b5ed8afce8
iamwjun <cddxwujun@hotmail.com>
2024-03-26 21:52:20 +0800

    feat: read file tree

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-use mini2react::PrimaryColor;
-use mini2react::mix;
+use mini2react::build_file_tree;
 
 fn main() {
-	let red = PrimaryColor::Red;
-	let yellow = PrimaryColor::Blue;
+    let file_tree = build_file_tree("/Users/wujun/MiniProjects/demo/components");
 
-	mix(red, yellow);
+    println!("{:?}", file_tree);
+
+    file_tree.traverse();
+
+    // create_or_replace_folder("widgets");
+
+    // let file_path = "widgets/index.tsx";
+
+    // let content = r#"
+    // // Your TypeScript content here
+    // const myVariable: string = "Hello, TypeScript!";
+    // console.log(myVariable);
+    // "#;
+
+    // match generate_typescript_default_export(content, file_path) {
+    //     Ok(_) => println!("TypeScript file '{}' generated successfully.", file_path),
+    //     Err(err) => eprintln!("Error generating TypeScript file: {}", err),
+    // }
+
+		// let _ = generate_react_function_component("Carousel", "widgets/Carousel.tsx");
 }
